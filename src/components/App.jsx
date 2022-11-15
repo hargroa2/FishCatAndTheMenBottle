7ff8c0e6d1e8e6e982593149c88ee49f54a5c68f
Amanda Hargrove <amandahargrove98@gmail.com>
2022-11-14 22:37:47 -0500

    Embedded youtube video

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,17 @@ function App() {
   return (
     <div>
       <Nav />
+      <iframe
+        width="958"
+        height="539"
+        src="https://www.youtube.com/embed/XUd2S8a2ChQ"
+        title="Catfish and the Bottlemen - Longshot"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+        className="longshotvideo"
+        autoplay
+      ></iframe>
     </div>
   );
 }
