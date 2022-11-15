2cc9832dc1aab84822477205a092df71230d35f9
Amanda Hargrove <amandahargrove98@gmail.com>
2022-11-14 22:05:00 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import "../styles/App.scss";
+import "../main";
+import "../fonts/P22 Johnston Underground Regular.ttf";
+import Nav from "../Nav.jsx";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
