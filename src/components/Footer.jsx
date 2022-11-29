c19f55af0ca2184c896f588c1c92405ec5732210
Amanda Hargrove <amandahargrove98@gmail.com>
2022-11-29 17:06:06 -0500

    Installed react-icons for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div>
-      <footer className="footer"></footer>
+      <footer>
+        <div className="icons">
+          <FaFacebookF color="white" />
+          <FaTwitter color="white" />
+        </div>
+        <div className="term">
+          <Link to="#" className="terms">
+            © 2022 ISLAND RECORDS
+          </Link>
+          <span className="footer-spacers"> | </span>
+          <Link to="#" className="terms">
+            COOKIES
+          </Link>
+          <span className="footer-spacers"> | </span>
+          <Link to="#" className="terms">
+            TERMS
+          </Link>
+          <span className="footer-spacers"> | </span>
+          <Link to="#" className="terms">
+            SAFE SURF
+          </Link>
+          <span className="footer-spacers"> | </span>
+          <Link to="#" className="terms">
+            PRIVACY
+          </Link>
+          <span className="footer-spacers"> | </span>
+          <Link to="#" className="terms">
+            COOKIE CHOICES
+          </Link>
+          <span className="footer-spacers"> | </span>
+          <Link to="#" className="terms">
+            DO NOT SELL MY PERSONAL INFORMATION
+          </Link>
+        </div>
+      </footer>
     </div>
   );
 };
