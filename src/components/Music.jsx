f1481bc466077b087a0dd9e4727418222a4496e3
Amanda Hargrove <amandahargrove98@gmail.com>
2023-01-09 18:29:52 -0500

    Added footer and nav to music component

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Footer from "./Footer";
+import NavShop from "./NavShop";
 
 const Music = () => {
-  return <div>Music</div>;
+  return (
+    <div>
+      <NavShop />
+      MUSIC
+      <Footer />
+    </div>
+  );
 };
 
 export default Music;
